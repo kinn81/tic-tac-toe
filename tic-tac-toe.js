17ee3eff97088518186d8286e09830c33674d55a
Keir Innes <keir.innes@gmail.com>
2021-11-14 12:40:33 +1300

    Update. Working on minimax solution..

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,17 +3,17 @@ const PlayerFactory = (name, symbol) => {
     symbol = symbol;
     getName = () => {
         return name;
-    }
+    };
 
     getSymbol = () => {
         return symbol;
-    }
+    };
 
     introduceSelf = () => {
         console.log('Hi, my name is ' + name + ' and my symbol is ' + symbol);
-    }
-    return { getName, getSymbol, introduceSelf }
-}
+    };
+    return { getName, getSymbol, introduceSelf };
+};
 
 /*************
     GameBoard
@@ -21,15 +21,15 @@ const PlayerFactory = (name, symbol) => {
     It exposes methods to clearBoard, makeMove, checkValidMove, printBoard, and checkForWinner 
 **************/
 const GameBoard = (() => {
-    let board = Array.apply(null, Array(8)).map(function () { })
+    let board = Array.apply(null, Array(9)).map(function () { });
     let movesMade = 0;
     let winner = undefined;
 
     const clearBoard = () => {
-        board = Array.apply(null, Array(8)).map(function () { });
+        board = Array.apply(null, Array(9)).map(function () { });
         winner = undefined;
         movesMade = 0;
-    }
+    };
 
     const makeMove = (symbol, position) => {
         if (winner) return;
@@ -38,65 +38,69 @@ const GameBoard = (() => {
             movesMade++;
             checkForWinner(symbol, position);
             return true;
-        }
+        };
         return false;
+    };
+
+    const getMoves = () => {
+        return moves;
     }
 
     const checkValidMove = (position) => {
         if (!board[position]) {
             return true
         } else return false;
-    }
+    };
 
     const checkForWinner = (symbol, position) => {
         if (checkRow(symbol, position) || checkCol(symbol, position) || checkDiag(symbol, position)) {
             winner = symbol;
-        }
-    }
+        };
+    };
 
     const getWinner = () => {
         return winner;
-    }
+    };
     /*******CODE TO CHECK FOR WINNING ROWS*********/
     const checkRow = (symbol, position) => {
         rowVar = Math.trunc(position / 3);
         for (i = rowVar * 3; i <= (rowVar * 3) + 2; i++) {
             if (board[i] != symbol) {
                 return false;
-            }
-        }
+            };
+        };
         return true;
-    }
+    };
 
     const checkCol = (symbol, position) => {
         colVar = position % 3;
         for (i = colVar; i <= colVar + 6; i = (i + 3)) {
             if (board[i] != symbol) {
                 return false;
-            }
-        }
+            };
+        };
         return true;
-    }
+    };
 
     const checkDiag = (symbol, position) => {
         if ('246'.includes(position)) {
-            if ([board[2], board[4], board[6]].every(v => v === board[2])) return true;;
-        }
+            if ([board[2], board[4], board[6]].every(v => v === board[2])) return true;
+        };
         if ('048'.includes(position)) {
-            if ([board[0], board[4], board[8]].every(v => v === board[0])) return true;;
-        }
+            if ([board[0], board[4], board[8]].every(v => v === board[0])) return true;
+        };
         return false;
-    }
+    };
     /******************************************** */
     const hasSomeoneWon = () => {
         if (winner) return true;
         return false;
-    }
+    };
 
     const isDraw = () => {
         if (!winner && movesMade === 9) return true;
         return false;
-    }
+    };
 
     const printBoard = () => {
         let boardString = '';
@@ -106,23 +110,23 @@ const GameBoard = (() => {
                     boardString += (board[(i * 3) + j])
                 } else {
                     boardString += '_';
-                }
+                };
                 if (j != 2) {
                     boardString += ' | '
                 } else {
                     boardString += '\n'
-                }
-            }
-        }
-        boardString += '\n'
+                };
+            };
+        };
+        boardString += '\n';
         return boardString;
-    }
+    };
 
     const getBoard = () => {
         return board.slice();
-    }
+    };
 
-    return { clearBoard, makeMove, checkValidMove, printBoard, getWinner, hasSomeoneWon, isDraw, getBoard };
+    return { clearBoard, makeMove, checkValidMove, printBoard, getWinner, hasSomeoneWon, isDraw, getBoard, getMoves };
 })();
 
 /*************  
@@ -133,17 +137,17 @@ const GameLogic = (() => {
 
     //VARIABLES TO HANDLE AN AI PLAYER
     const aiSwitch = document.getElementById('aiSwitch');
-    let computerAI =  aiSwitch.checked;
+    let computerAI = aiSwitch.checked;
     const aiText = document.getElementById('aiText');
     aiSwitch.onclick = () => {
         computerAI = aiSwitch.checked;
-        if(computerAI) {
+        if (computerAI) {
             aiText.style.color = '#2196F3';
         } else {
             aiText.style.color = 'darkgray';
-        }
-        if(currentPlayer.getName() === 'P2' && (!GameBoard.getWinner() && !GameBoard.isDraw())) makeComputerMove();
-    }
+        };
+        if (currentPlayer.getName() === 'P2' && (!GameBoard.getWinner() && !GameBoard.isDraw())) makeAIMove();
+    };
 
     /******VARIABLES FOR PLAYER OBJECTS******/
     let tempPlayer;
@@ -164,6 +168,7 @@ const GameLogic = (() => {
         resetGame();
     });
 
+
     //******CODE TO HANDLE USER INPUT******/
     function takeInput(event) {
         if (GameBoard.makeMove(currentPlayer.getSymbol(), event.target.dataset.id)) {
@@ -171,20 +176,21 @@ const GameLogic = (() => {
             nextPlayerTurn();
             if (checkGameOver()) {
                 return;
-            }
-            if (computerAI && currentPlayer.getName() === 'P2') makeComputerMove();
-        }
-    }
+            };
+            //if (computerAI && currentPlayer.getName() === 'P2') makeComputerMoveRandom();
+            if (computerAI && currentPlayer.getName() === 'P2') makeAIMove();
+        };
+    };
 
     //AFTER A TURN IS TAKEN SWAP THE CURRENT/NEXT PLAYERS
     const nextPlayerTurn = () => {
         tempPlayer = currentPlayer;
         currentPlayer = nextPlayer;
         nextPlayer = tempPlayer;
-    }
+    };
 
-    /**********LOGIC FOR RANDOM COMPUTER PLAYER******* */
-    const makeComputerMove = () => {
+    /**********LOGIC FOR RANDOM COMPUTER MOVE******* */
+    const makeComputerMoveRandom = () => {
         const tempBoard = GameBoard.getBoard();
         while (true) {
             const move = Math.floor(Math.random() * 9);
@@ -193,47 +199,79 @@ const GameLogic = (() => {
                 nextPlayerTurn();
                 checkGameOver();
                 return;
+            };
+        };
+    };
+
+
+    /**********LOGIC FOR MINIMAX DETERMINED COMPUTER PLAYER******* */
+    const makeAIMove = () => {
+        const tempBoard = GameBoard.getBoard();
+        let bestScore = -Infinity;
+        let bestMove = null;
+
+        for (i = 0; i < tempBoard.length; i++) {
+            if (tempBoard[i] === undefined) {
+                tempBoard[i] = currentPlayer.getSymbol();
+                let score = miniMax(tempBoard, true);
+                tempBoard[i] = undefined;
+                console.log('   !!!!!!!!!score: ' + score);
+                if (score > bestScore) {
+                    bestScore = score;
+                    bestMove = i;
+                }
             }
-            console.log('loop');
         }
-    }
+        GameBoard.makeMove(currentPlayer.getSymbol(), bestMove);
+        cellContainer.querySelector(`[data-id='${bestMove}']`).textContent = currentPlayer.getSymbol();
+        nextPlayerTurn();
+        checkGameOver();
+    };
+
+    const miniMax = (board, isMaximizing) => {
+        if(board.getMoves() === 9) {
+            
+        }
+        return 1;
+    };
+
 
     //CHECK FOR A WINNER OR DRAW
     const checkGameOver = () => {
         if (GameBoard.getWinner()) {
             gameIsFinished(`${GameBoard.getWinner()} WINS THE GAME!!`);
             return true;
-        }
+        };
         if (GameBoard.isDraw()) {
             gameIsFinished(`IT'S A DRAW!!`);
             return true;
-        }
+        };
         return false;
-    }
+    };
 
     //WHEN THE RESET BUTTON IS PRESSED
     const resetGame = () => {
         GameBoard.clearBoard();
         clearUIBoard();
-        gameOverDialog.textContent = ``
+        gameOverDialog.textContent = ``;
         gameOverDialog.style.display = 'none';
         tempPlayer = undefined;
         currentPlayer = PlayerFactory('P1', 'X');
         nextPlayer = PlayerFactory('P2', 'O');
-    }
+    };
 
     //RESET THE HTML UI
     const clearUIBoard = () => {
         for (i = 0; i < cells.length; i++) {
             cells[i].textContent = '';
-        }
-    }
+        };
+    };
 
     //DISPLAY A MESSAGE WHEN THE GAME IS OVER
     const gameIsFinished = (m) => {
         gameOver.textContent = m;
         gameOver.style.display = 'block';
-    }
+    };
 
 })();
 
